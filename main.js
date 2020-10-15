a6118e69198f4b5716691bf9bc6ce74cafc0d307
Małgorzata Ordak <malgorzata_ordak@wp.pl>
2020-10-15 14:35:14 +0200

    added the classes

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
-alert ('Cześć! :) Miło Cię widzieć')
-console.log( 'Siemanko, Siemanko! Witaj na mojej stronie. Prawda, że świetna? :D') 
+alert ('Cześć! :) Miło Cię widzieć');
+
+console.log( 'Siemanko, Siemanko! Witaj na mojej stronie. Prawda, że świetna? :D');
+
+const h1 = document.querySelector('.malinowy__h1--js');
+
+h1.innerHTML = 'Siemanko, Siemanko! Witaj na mojej stronie. Prawda, że świetna? :D'
