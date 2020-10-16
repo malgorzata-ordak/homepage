217b26ec5f98d2fc227de8bbdad064aef5d62144
Małgorzata Ordak <malgorzata_ordak@wp.pl>
2020-10-16 14:30:16 +0200

    added new paragraph by JS

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ console.log( 'Siemanko, Siemanko! Witaj na mojej stronie. Prawda, że świetna? 
 const h1 = document.querySelector('.malinowy__h1--js');
 
 h1.innerHTML = 'Siemanko, Siemanko! Witaj na mojej stronie. Prawda, że świetna? :D'
+
+const pustyParagraf = document.querySelector('.pomysł__opis--js');
+pustyParagraf.innerHTML = `Ten element został stworzony i uzupełniony za pomocą JS. Jestem z tego bardzo dumna :)`
