9d9bf3bc59c5f9f12c22e5cad4966095cdbbed92
Małgorzata Ordak <malgorzata_ordak@wp.pl>
2020-10-26 13:38:12 +0100

    background changed, normalize.css added

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+const firstName = "Gosia"
+const age = 31
+
 alert ('Cześć! :) Miło Cię widzieć');
 
-console.log( 'Siemanko, Siemanko! Witaj na mojej stronie. Prawda, że świetna? :D');
+console.log( `Siemanko, Siemanko! Witaj na mojej stronie. Prawda, że świetna? :D Jestem ${firstName} i mam ${age} lat`);
 
 const h1 = document.querySelector('.malinowy__h1--js');
 
