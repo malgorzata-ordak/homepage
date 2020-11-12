5e89bf199a05f848ab77c591bf386986cddaf89e
Małgorzata Ordak <malgorzata_ordak@wp.pl>
2020-11-12 12:18:12 +0100

    added grid, media queries

--- main.js
+++ main.js
@@ -11,3 +11,22 @@ h1.innerHTML = 'Siemanko, Siemanko! Witaj na mojej stronie. Prawda, że świetna
 
 const pustyParagraf = document.querySelector('.pomysł__opis--js');
 pustyParagraf.innerHTML = `Ten element został stworzony i uzupełniony za pomocą JS. Jestem z tego bardzo dumna :)`
+
+
+function greet (firstName, age) {
+    console.log (
+        `Witaj drogi Gościu. Nazywam się ${firstName} i mam ${age} lat`);
+}
+
+greet(firstName, age);
+
+const greetTwo = (firstName, age) => {
+console.log (
+    `Witaj witaj drogi Gościu. Mam na imię ${firstName} i mam ${age} lat`);
+
+}
+
+greetTwo('Gosia', '31');
+
+
+
