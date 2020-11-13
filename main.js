a2dcdf0298d8c78106d42018ef42007139bd5795
Małgorzata Ordak <malgorzata_ordak@wp.pl>
2020-11-13 13:47:14 +0100

    addEventListener<!DOCTYPE html>

--- main.js
+++ main.js
@@ -1,16 +1,13 @@
-const firstName = "Gosia"
-const age = 31
+const firstName = "Gosia";
+const age = 31;
 
 alert ('Cześć! :) Miło Cię widzieć');
 
 console.log( `Siemanko, Siemanko! Witaj na mojej stronie. Prawda, że świetna? :D Jestem ${firstName} i mam ${age} lat`);
 
-const h1 = document.querySelector('.malinowy__h1--js');
-
-h1.innerHTML = 'Siemanko, Siemanko! Witaj na mojej stronie. Prawda, że świetna? :D'
 
 const pustyParagraf = document.querySelector('.pomysł__opis--js');
-pustyParagraf.innerHTML = `Ten element został stworzony i uzupełniony za pomocą JS. Jestem z tego bardzo dumna :)`
+pustyParagraf.innerHTML = `Ten element został stworzony i uzupełniony za pomocą JS. Jestem z tego bardzo dumna :)`;
 
 
 function greet (firstName, age) {
@@ -29,4 +26,15 @@ console.log (
 greetTwo('Gosia', '31');
 
 
+const button = document.querySelector('.action--js');
+console.log(button)
+ 
+const myClick = () => {
+const h1 = document.querySelector('.malinowy__h1--js');
+h1.innerHTML = `Siemanko, Siemanko! Witaj na mojej stronie. Prawda, że świetna? :D`;
+h1.classList.toggle( 'klasa-z-js');
+
+}
+
+ button.addEventListener('click', myClick);
 
